24d90a7f312d10defc77032c2fa2bcad1bd6ccff
Ubuntu <ubuntu@ip-172-31-38-142.eu-north-1.compute.internal>
2024-04-16 07:34:26 +0000

    feat: adding Java app

---
+++ java-app/Hello.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Dosto! Mere nam Sushil hai");
+		Date currentDate = new Date();
+		System.out.println("Today's date is "+ currentDate);
+	}
+}
